7fb7ee66450e1561f716ad656c5fae1fc81a3ce6
Zhuhc <653818011@qq.com>
2017-05-23 21:11:27 +0800

    Initial commit

---
+++ app/src/main/java/cn/xuemcu/password/MainActivity.java
@@ -0,0 +1,25 @@
+package cn.xuemcu.password;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnTest = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnTest = (Button) findViewById(R.id.btnTest);
+        btnTest.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
+    }
+}
