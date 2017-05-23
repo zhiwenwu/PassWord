ea2f39cd84f9965de450fbbfef6ebff4a60f0025
Zhuhc <653818011@qq.com>
2017-05-23 22:49:54 +0800

    Test

--- app/src/main/java/cn/xuemcu/password/MainActivity.java
+++ app/src/main/java/cn/xuemcu/password/MainActivity.java
@@ -1,13 +1,16 @@
 package cn.xuemcu.password;
 
+import android.support.v4.hardware.fingerprint.FingerprintManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnTest = null;
+    private FingerprintManagerCompat manager = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +19,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnTest = (Button) findViewById(R.id.btnTest);
         btnTest.setOnClickListener(this);
+
+        // 获取一个FingerPrintManagerCompat的实例
+        manager = FingerprintManagerCompat.from(this);
     }
 
     @Override
     public void onClick(View v) {
         //测试程序Test
         Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
+        manager.authenticate(null, 0, null, new MyCallBack(), null);
+    }
+
+    public class MyCallBack extends FingerprintManagerCompat.AuthenticationCallback {
+        private static final String TAG = "MyCallBack";
+
+        // 当出现错误的时候回调此函数，比如多次尝试都失败了的时候，errString是错误信息
+        @Override
+        public void onAuthenticationError(int errMsgId, CharSequence errString) {
+            Log.d(TAG, "onAuthenticationError: " + errString);
+        }
+
+        // 当指纹验证失败的时候会回调此函数，失败之后允许多次尝试，失败次数过多会停止响应一段时间然后再停止sensor的工作
+        @Override
+        public void onAuthenticationFailed() {
+            Log.d(TAG, "onAuthenticationFailed: " + "验证失败");
+        }
+
+        @Override
+        public void onAuthenticationHelp(int helpMsgId, CharSequence helpString) {
+            Log.d(TAG, "onAuthenticationHelp: " + helpString);
+        }
+
+        // 当验证的指纹成功时会回调此函数，然后不再监听指纹sensor
+        @Override
+        public void onAuthenticationSucceeded(FingerprintManagerCompat.AuthenticationResult
+                                                      result) {
+            Log.d(TAG, "onAuthenticationSucceeded: " + "验证成功");
+        }
     }
 }
