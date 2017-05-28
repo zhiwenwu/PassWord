04d831fec61205fc80df9ddf62051ca3ca15dbff
Zhuhc <653818011@qq.com>
2017-05-28 19:40:23 +0800

    Test

--- app/src/main/java/cn/xuemcu/password/MainActivity.java
+++ app/src/main/java/cn/xuemcu/password/MainActivity.java
@@ -1,65 +1,14 @@
 package cn.xuemcu.password;
 
-import android.support.v4.hardware.fingerprint.FingerprintManagerCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btnTest = null;
-    private FingerprintManagerCompat manager = null;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btnTest = (Button) findViewById(R.id.btnTest);
-        btnTest.setOnClickListener(this);
-
-        // 获取一个FingerPrintManagerCompat的实例
-        manager = FingerprintManagerCompat.from(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        //测试程序Test
-        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
-        manager.authenticate(null, 0, null, new MyCallBack(), null);
-    }
-
-    public class MyCallBack extends FingerprintManagerCompat.AuthenticationCallback {
-        private static final String TAG = "MyCallBack";
-
-        // 当出现错误的时候回调此函数，比如多次尝试都失败了的时候，errString是错误信息
-        @Override
-        public void onAuthenticationError(int errMsgId, CharSequence errString) {
-            Log.d(TAG, "onAuthenticationError: " + errString);
-            Toast.makeText(MainActivity.this, "onAuthenticationError: " + errString, Toast.LENGTH_SHORT).show();
-        }
-
-        // 当指纹验证失败的时候会回调此函数，失败之后允许多次尝试，失败次数过多会停止响应一段时间然后再停止sensor的工作
-        @Override
-        public void onAuthenticationFailed() {
-            Log.d(TAG, "onAuthenticationFailed: " + "验证失败");
-            Toast.makeText(MainActivity.this, "onAuthenticationFailed: " + "验证失败", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onAuthenticationHelp(int helpMsgId, CharSequence helpString) {
-            Log.d(TAG, "onAuthenticationHelp: " + helpString);
-            Toast.makeText(MainActivity.this, "onAuthenticationHelp: " + helpString, Toast.LENGTH_SHORT).show();
-        }
-
-        // 当验证的指纹成功时会回调此函数，然后不再监听指纹sensor
-        @Override
-        public void onAuthenticationSucceeded(FingerprintManagerCompat.AuthenticationResult result) {
-            Log.d(TAG, "onAuthenticationSucceeded: " + "验证成功");
-            Toast.makeText(MainActivity.this, "onAuthenticationSucceeded: " + "验证成功", Toast.LENGTH_SHORT).show();
-            Toast.makeText(MainActivity.this, "onAuthenticationSucceeded: " + "123123", Toast.LENGTH_SHORT).show();
-        }
-    }
 }
