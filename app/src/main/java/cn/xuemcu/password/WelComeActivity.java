8fb759c06a73dc1fb98a5248645ab6c56cd9672a
Zhuhc <653818011@qq.com>
2017-05-28 18:48:32 +0800

    Test

--- app/src/main/java/cn/xuemcu/password/WelComeActivity.java
+++ app/src/main/java/cn/xuemcu/password/WelComeActivity.java
@@ -1,17 +1,19 @@
 package cn.xuemcu.password;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 /**
  * Created by 朱红晨 on 2017/5/28.
  */
 
-public class WelComeActivity extends AppCompatActivity {
+public class WelComeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome);
     }
 }
