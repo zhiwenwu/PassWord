ae54a93a242482214febaa7670b2f992e50e28f0
Zhuhc <653818011@qq.com>
2017-05-29 00:21:34 +0800

    Test

--- app/src/main/java/cn/xuemcu/password/MainActivity.java
+++ app/src/main/java/cn/xuemcu/password/MainActivity.java
@@ -11,6 +11,7 @@ import java.util.Calendar;
 
 public class MainActivity extends Activity implements Runnable {
     private static final int MainActivity_TOAST = 0;
+    private static final int MainActivity_PASS = 1;
     private boolean threadFlag = true;
     private EditText editText = null;
     private Message message = null;
@@ -31,12 +32,14 @@ public class MainActivity extends Activity implements Runnable {
     private Handler mHandler = new Handler(){
         public void handleMessage(android.os.Message msg) {
             switch (msg.what) {
-                case MainActivity_TOAST:
-                    //Toast.makeText(MainActivity.this,msg.obj.toString(),Toast.LENGTH_SHORT).show();
+                case MainActivity_PASS:
                     Intent intent = new Intent();
                     intent.setClass(MainActivity.this, PassWordActivity.class);
                     startActivity(intent);
                     finish();
+                    break;
+                case MainActivity_TOAST:
+                    Toast.makeText(MainActivity.this,msg.obj.toString(),Toast.LENGTH_SHORT).show();
                     break;
                 default:
                     break;
@@ -53,12 +56,22 @@ public class MainActivity extends Activity implements Runnable {
                 e.printStackTrace();
             }
 
-            String str = Integer.toString(calendar.get(Calendar.HOUR_OF_DAY));
-            str += Integer.toString(calendar.get(Calendar.MINUTE));
+            String str = "";
+            //防止时或分是一位数，导致密码不是四位数字
+            if(calendar.get(Calendar.HOUR_OF_DAY) < 10)
+                str += "0" + Integer.toString(calendar.get(Calendar.HOUR_OF_DAY));
+            else
+                str += Integer.toString(calendar.get(Calendar.HOUR_OF_DAY));
+
+            if(calendar.get(Calendar.MINUTE) < 10)
+                str += "0" + Integer.toString(calendar.get(Calendar.MINUTE));
+            else
+                str += Integer.toString(calendar.get(Calendar.MINUTE));
+
             if(editText.getText().toString().equals(str)) {
                 this.threadFlag = false;
 
-                message.what = MainActivity_TOAST;
+                message.what = MainActivity_PASS;
                 message.obj = "密码正确";
                 this.mHandler.sendMessage(this.message);
             }
