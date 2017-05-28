5a181fe551e54c258a4419e3f0a07106d2a5ff0c
Zhuhc <653818011@qq.com>
2017-05-28 18:57:31 +0800

    Test

--- app/src/main/java/cn/xuemcu/password/WelComeActivity.java
+++ app/src/main/java/cn/xuemcu/password/WelComeActivity.java
@@ -1,19 +1,38 @@
 package cn.xuemcu.password;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
+import android.os.Handler;
 
 /**
  * Created by 朱红晨 on 2017/5/28.
  */
 
 public class WelComeActivity extends Activity {
+    private static final int GOTO_MAIN_ACTIVITY = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        //mHandler.sendEmptyMessageAtTime(GOTO_MAIN_ACTIVITY, 3000);//3秒跳转
     }
+
+    private Handler mHandler = new Handler(){
+        public void handleMessage(android.os.Message msg) {
+            switch (msg.what) {
+                case GOTO_MAIN_ACTIVITY:
+                    Intent intent = new Intent();
+                    intent.setClass(WelComeActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    break;
+
+                default:
+                    break;
+            }
+        };
+    };
 }
