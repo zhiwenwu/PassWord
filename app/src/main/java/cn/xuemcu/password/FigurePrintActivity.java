d53fd0d52c0f4d3ade4f35f6633d65c4e66f14bb
Zhuhc <653818011@qq.com>
2017-05-31 02:13:23 +0800

    Test

--- app/src/main/java/cn/xuemcu/password/FigurePrintActivity.java
+++ app/src/main/java/cn/xuemcu/password/FigurePrintActivity.java
@@ -3,14 +3,20 @@ package cn.xuemcu.password;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.ant.liao.GifView;
+
 /**
  * Created by 朱红晨 on 2017/5/30.
  */
 
 public class FigurePrintActivity extends Activity {
+    private GifView gifView = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_figureprint);
+
+        gifView = (GifView) findViewById(R.id.gifView);
+        gifView.setGifImage(R.drawable.zhiwen);
     }
 }
